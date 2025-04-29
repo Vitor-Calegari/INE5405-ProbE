8ec7a4cccece98a457fba6192d23307dd5f02f9e
Vitor Calegari <vitorprca@gmail.com>
2025-04-29 14:46:48 -0300

    Concerta average viewers

--- scripts_python/average_viewers_boxplot.py
+++ scripts_python/average_viewers_boxplot.py
@@ -32,7 +32,7 @@ def main():
     
     # Boxplot sem os outliers extremos
     fig.add_trace(go.Box(
-        y=filtered_average_viewers,
+        y=average_viewers,
         name='Average viewers (sem outliers)',
         boxpoints=False,
         hovertext=twitchdata['Channel'],
@@ -40,7 +40,6 @@ def main():
     ), row=1, col=2,)
     
     # Atualiza o layout e mostra a figura
-    fig.update_layout(yaxis_range=[-4,4])
     fig.update_layout(title='Average viewers', showlegend=False)
     fig.update_yaxes(title_text='Average viewers', row=1, col=1)
     fig.update_yaxes(title_text='Average viewers', row=1, col=2)
