9359f64e219e7dfe4466c0f28bc037cbebd8ff6b
Antônio Torres <antonio.escobar.torres11@gmail.com>
2025-05-03 23:25:13 -0300

    corrigida linha de regressã olinear

--- scripts_python/relationFollowersXPeakViewers.py
+++ scripts_python/relationFollowersXPeakViewers.py
@@ -7,45 +7,33 @@ import matplotlib.ticker as ticker
 import seaborn as sns
 
 def relationFollowersXPeakViewers(twitchdata):
-    # Cria a figura
     plt.figure(figsize=(10, 6))
 
-    # Plota o scatter + linha de regressão linear
-    sns.regplot(
+    sns.scatterplot(
         data=twitchdata,
         x='Followers',
         y='Peak viewers',
-        scatter_kws={'alpha': 0.5, 'color': 'mediumseagreen', 'edgecolors': 'black', 'linewidths': 0.3},
-        line_kws={'color': 'darkblue', 'label': 'Tendência Linear',
-        },
-        ci = None
-        )
+        alpha=0.5,
+        color='mediumseagreen',
+        edgecolor='black',
+        linewidth=0.3
+    )
 
-    # Título e eixos
     plt.title('Followers × Peak Viewers', fontsize=14)
     plt.xlabel('Seguidores', fontsize=12)
     plt.ylabel('Pico de Visualizações', fontsize=12)
 
-    # Formata o eixo Y
     plt.gca().yaxis.set_major_locator(ticker.MultipleLocator(50000))
     plt.gca().yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, _: f'{int(x/1000)}k'))
 
-    # Limita os eixos (remove outliers)
-    plt.xlim(0, twitchdata['Followers'].quantile(0.99))  # Limita até 99% dos seguidores
-    plt.ylim(0, twitchdata['Peak viewers'].quantile(0.99))  # Limita até 99% do pico de visualizações
+    plt.xlim(0, twitchdata['Followers'].quantile(0.99))
+    plt.ylim(0, twitchdata['Peak viewers'].quantile(0.99))
 
-    # Formata o eixo X
     plt.gca().xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, _: f'{int(x/1000)}k'))
 
-
-    # Formata o eixo Y
-
-    # Exibe o gráfico
     plt.grid(True, linestyle='--', alpha=0.6)
-    plt.legend()
     plt.tight_layout()
     plt.show()
-
 
 def main():
 
